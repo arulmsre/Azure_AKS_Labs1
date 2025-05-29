ca33101c1a869e26103411c707fcea284721b00a
arulmsre <arul.sre@outlook.com>
2025-05-28 20:05:47 -0400

    Update main.tf

--- AKS/main.tf
+++ AKS/main.tf
@@ -1,31 +1,28 @@
-# Generate random resource group name
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = random_pet.rg_name.id
+resource "azurerm_resource_group" "petclinic_rg" {
+  name     = "petclinic-rg"
+  location = "East US"
 }
 
-resource "random_pet" "azurerm_kubernetes_cluster_name" {
-  prefix = "cluster"
-}
+resource "azurerm_kubernetes_cluster" "petclinic_aks" {
+  name                = "petclinic-aks"
+  location           = azurerm_resource_group.petclinic_rg.location
+  resource_group_name = azurerm_resource_group.petclinic_rg.name
+  dns_prefix         = "petclinic"
 
-resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
-  prefix = "dns"
-}
-
-resource "azurerm_kubernetes_cluster" "k8s" {
-  location            = azurerm_resource_group.rg.location
-  name                = random_pet.azurerm_kubernetes_cluster_name.id
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
-  kubernetes_version  = "1.30.0"
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
 
   identity {
     type = "SystemAssigned"
   }
+}
 
   default_node_pool {
     name       = "agentpool"
